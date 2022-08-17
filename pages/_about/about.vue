387eabaedd1a3c0f6f4119f45230ee75209aa5fd
Samuel Zwinglio <samuelzwinglio@gmail.com>
2022-08-17 12:50:23 -0300

    first commit

---
+++ pages/_about/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>
+
+
+ {{ nome }}
+    </h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            "nome" : this.$route.param.about,
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <h1>Olá</h1>
+    <NuxtLink to="about">Sobre</NuxtLink>
+    <HeadingLogout />
+  </div>
+</template>
+
+<script>
+import HeadingLogout from '../components/HeadingLogout.vue';
+export default {
+    name: "IndexPage",
+    components: { HeadingLogout }
+}
+</script>
