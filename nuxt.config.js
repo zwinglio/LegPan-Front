a97432b6a01c1251a37864deba01b5ce9c550eb8
Samuel Zwinglio <samuelzwinglio@gmail.com>
2022-12-23 10:33:21 -0300

    added hability to customize bootstrap and start login page

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    "@/assets/scss/bootstrap-config.scss",
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -21,7 +23,7 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: ["@nuxtjs/style-resources"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
@@ -31,6 +33,11 @@ export default {
     "@nuxtjs/pwa",
     "bootstrap-vue/nuxt",
   ],
+
+  bootstrapVue: {
+    bootstrapCSS: false, // Or `css: false`
+    bootstrapVueCSS: false, // Or `bvCSS: false`
+  },
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
---
+++ pages/login.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="app">
+    <section id="menu" class="bg-dark py-3">
+      <b-container>
+        <b-row>
+          <b-col>
+            <img src="~/assets/img/logo_light.svg" alt="Logo LegPlan" />
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+    <section id="conteudo" class="py-4">
+      <b-container>
+        <b-row align-v="center" align-h="center" style="height: 50vh">
+          <b-col lg="8">
+            <b-card no-body class="overflow-hidden">
+              <b-row no-gutters align-v="center" align-h="center">
+                <b-col
+                  md="4"
+                  class="text-center"
+                  style="background-color: #f1f1f1"
+                >
+                  <b-card-img
+                    src="~/assets/img/logo_color.svg"
+                    alt="Image"
+                    class="rounded-0 w-75"
+                    style="padding: 150px 0"
+                  ></b-card-img>
+                </b-col>
+                <b-col md="8">
+                  <b-card-body title="Login">
+                    <b-card-text>
+                      <form action="#">
+                        <!-- crie dois inputs, e-mail e senha -->
+                        <b-form-group
+                          id="input-group-1"
+                          label="E-mail:"
+                          label-for="input-1"
+                        >
+                          <b-form-input
+                            id="input-1"
+                            type="email"
+                            required
+                            placeholder="Digite seu e-mail"
+                          ></b-form-input>
+                        </b-form-group>
+                        <b-form-group
+                          id="input-group-2"
+                          label="Senha:"
+                          label-for="input-2"
+                        >
+                          <b-form-input
+                            id="input-2"
+                            type="password"
+                            required
+                            placeholder="Digite sua senha"
+                          ></b-form-input>
+                        </b-form-group>
+                        <b-button type="submit" variant="warning"
+                          >Entrar</b-button
+                        >
+                      </form>
+                    </b-card-text>
+                  </b-card-body>
+                </b-col>
+              </b-row>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+  </div>
+</template>
