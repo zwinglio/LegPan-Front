2f6dab7478bc8cef0746e2edda0b8076420ecd47
Samuel Zwinglio <samuelzwinglio@gmail.com>
2023-01-30 13:54:00 -0300

    change title and try to autoimport pinia

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "legplan-front",
+    title: "LegPlan - Planejamento Estratégico",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
@@ -12,12 +12,12 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "@/assets/scss/bootstrap-config.scss",
-  ],
+  css: ["@/assets/scss/bootstrap-config.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    // "~/plugins/axios",
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -25,8 +25,14 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     "@nuxtjs/style-resources",
-    '@nuxtjs/composition-api/module',
-    ['@pinia/nuxt', { disableVuex: true }]
+    "@nuxtjs/composition-api/module",
+    [
+      "@pinia/nuxt",
+      {
+        disableVuex: true,
+        autoImports: ['defineStore'],
+      },
+    ],
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -46,7 +52,6 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: "http://localhost:8000",
   },
 
